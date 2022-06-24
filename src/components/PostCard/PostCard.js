a830912a1c26c12889e8c31aa8cc642ce908f06c
megha-pathak <meghapathak2013@gmail.com>
2022-06-24 19:01:31 +0530

    add icons to Post card

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -2,72 +2,90 @@ import "./PostCard.css";
 
 import { useState } from "react";
 
-import { Instagram } from "react-content-loader";
 import { useNavigate } from "react-router-dom";
+import useDarkMode from "use-dark-mode";
 
 export const PostCard = (props) => {
-  let navigate = useNavigate();
+  const navigate = useNavigate();
+  const darkMode = useDarkMode(false);
   const [ispostLiked, setIsPostLiked] = useState(false);
-  const MyFacebookLoader = () => {
-    <Instagram />;
-  };
+
   const { postData } = props;
-  const { id, caption, imageURL, username, profilePic, likes } = postData;
+  const { caption, imageURL, username, profilePic, likes } = postData;
   const handleClick = () => {
-    navigate(`/profile/:${id}`);
+    navigate(`/profile/${username}`);
   };
   const handleLiked = () => {
     setIsPostLiked(!ispostLiked);
   };
+
   return (
-    <div className="post-card">
-      <div className="post-card-user-info" onClick={handleClick}>
-        <img className="user-photo" src={profilePic} alt="userPhoto" />
-        <span className="username">{username}</span>
-      </div>
-      <img
-        className="user-post"
-        src={imageURL}
-        alt="post"
-        onLoad={MyFacebookLoader}
-      />
-      <div className="pc201PopularityIcon">
+    <>
+      <div className="post-card">
+        <div className="post-card-user-info" onClick={handleClick}>
+          <img className="user-photo" src={profilePic} alt="userPhoto" />
+          <span className="username">{username}</span>
+        </div>
+        <img className="user-post" src={imageURL} alt="luffy" />
+        <div className="poc201popularityIcon">
+          {ispostLiked ? (
+            <img
+              className="insta-icon like-icon"
+              src="https://img.icons8.com/ios-filled/50/fa314a/like--v1.png"
+              alt="to-be-liked"
+              onClick={handleLiked}
+            />
+          ) : (
+            <img
+              className="insta-icon like-icon"
+              src={
+                darkMode.value
+                  ? "https://img.icons8.com/ios/28/ffffff/like--v1.png"
+                  : "https://img.icons8.com/ios/28/000000/like--v1.png"
+              }
+              alt="liked"
+              onClick={handleLiked}
+            />
+          )}
+          <img
+            className="insta-icon"
+            src={
+              darkMode.value
+                ? "https://img.icons8.com/ios/50/ffffff/topic.png"
+                : "https://img.icons8.com/ios/50/000000/topic.png"
+            }
+            alt="comment"
+          />
+          <img
+            className="insta-icon"
+            src={
+              darkMode.value
+                ? "https://img.icons8.com/ios/50/ffffff/sent.png"
+                : "https://img.icons8.com/ios/50/000000/sent.png"
+            }
+            alt="profile"
+          />
+          <img
+            className="insta-icon save-icon"
+            src={
+              darkMode.value
+                ? "https://img.icons8.com/external-bearicons-detailed-outline-bearicons/64/ffffff/external-Save-social-media-bearicons-detailed-outline-bearicons.png"
+                : "https://img.icons8.com/external-bearicons-detailed-outline-bearicons/64/000000/external-Save-social-media-bearicons-detailed-outline-bearicons.png"
+            }
+            alt="save"
+          />
+        </div>
         {ispostLiked ? (
-          <img
-            className="insta-icon like-icon"
-            src={""}
-            alt="to-be-liked"
-            onClick={handleLiked}
-          />
+          <div className="post-like-count">{likes + 1} Likes</div>
         ) : (
-          <img
-            className="insta-icon like-icon"
-            src={""}
-            alt="liked"
-            onClick={handleLiked}
-          />
+          <div className="post-like-count">{likes} Likes</div>
         )}
-        <img className="insta-icon" src={""} alt="comment" />
-        <img className="insta-icon" src={""} alt="profile" />
-        <img className="insta-icon save-icon" src={""} alt="save" />
+        <div className="post-caption">
+          <span className="username ">{username}</span>
+          <span className="caption-description">{caption}</span>
+        </div>
+        <div className="post-date">19 Feb, 2021</div>
       </div>
-      {ispostLiked ? (
-        <div className="post-like-count">{likes + 1} Likes</div>
-      ) : (
-        <div className="post-like-count">{likes} Likes</div>
-      )}
-      <div className="post-caption">
-        <span className="username ">{username}</span>
-        <span className="caption-description">{caption}</span>
-      </div>
-      <div className="post-date">28 December 2021</div>
-      <div className="pc201PostAddComment">
-        <input
-          className="comment-box"
-          type="text"
-          placeholder="Add a comment..."
-        />
-      </div>
-    </div>
+    </>
   );
 };
